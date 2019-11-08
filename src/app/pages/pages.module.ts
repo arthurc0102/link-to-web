530fe3581da2666acdfb80198cafc1f08ce07811
Arthur Chang <arthurc0102@gmail.com>
2019-11-08 18:00:27 +0800

    style(pages): add line

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ComponentsModule } from '../components/components.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 
