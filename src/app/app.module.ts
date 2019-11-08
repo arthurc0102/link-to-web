43166e0bc7c4946ccca918367c4d1c83efbd1105
Arthur Chang <arthurc0102@gmail.com>
2019-11-08 12:59:30 +0800

    chore(prject): add folder for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
