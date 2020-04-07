c93d586df48d24c2df1d9afafe45725854b984aa
Arthur Chang <arthurc0102@gmail.com>
2020-04-07 14:05:15 +0800

    feat(spinner): add ngx-spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  styles: [`
+    .spinner-text {
+      font-size: 20px;
+      color: white;
+    }
+  `],
   template: `
     <app-toolbar></app-toolbar>
     <router-outlet></router-outlet>
+    <ngx-spinner
+      bdColor="#333"
+      size="large"
+      color="#fff"
+      type="pacman"
+      [fullScreen]="true"
+    >
+      <p class="spinner-text">Loading...</p>
+    </ngx-spinner>
   `,
 })
 export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxSpinnerModule } from "ngx-spinner";
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -19,6 +20,7 @@ import { MaterialModule } from './modules/material.module';
     BrowserAnimationsModule,
     ComponentsModule,
     MaterialModule,
+    NgxSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
